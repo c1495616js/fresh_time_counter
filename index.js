9aea085b7fec35fe5efafde9e7d283b617acb949
Jerry Wang <jerry@freshworks.io>
2021-03-03 13:05:24 -0800

    fix timezone

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ async function getChannelUsers(channel) {
 async function getMessages(channel) {
   const todayDate = moment().tz('America/Los_Angeles').format('YYYY-MM-DD');
   // const timezoneDiff = 25200;
-  const timezoneDiff = 0;
+  const timezoneDiff = 28800;
 
   const oldest =
     moment(todayDate + 'T00:00:00')
